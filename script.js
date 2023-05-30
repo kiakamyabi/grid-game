438da160684cc2680d90c3402df11331d6374897
kiakamyabi <114905508+kiakamyabi@users.noreply.github.com>
2023-05-30 03:40:34 +0100

    Event listener more dynamic. Beginning of cell features stored in object.
    Event listener more dynamic by being based on the grid itself rather than individual cells. Beginning of cell features stored in object for terrain and buildings.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const board = document.getElementById("game-grid")
+//Store terrain as object along with cell features
+//Solve problem of multiple buildings. Maybe slots?
+const gameGrid = document.getElementById('game-grid')
+const table = document.createElement('table');
+//Configs
 const numRows = 5;
 const numCols = 5;
-const table = document.createElement('table');
 
-const buildings = {
-    'Farm': 50,
-    'Mine': 100,
-    'Shop': 200
-  };
+const terrain = {};
 
-//World Map
+// const buildings = {
+//     'Farm': 50,
+//     'Mine': 100,
+//     'Shop': 200
+//   };
+
+//World Map Generation
 for (let row = 0; row < numRows; row++) {
     const newRow = document.createElement('tr');
     for (let col = 0; col < numCols; col++) {
@@ -19,22 +24,36 @@ for (let row = 0; row < numRows; row++) {
       colPlusOne = col + 1;
       individualCell.id = `cell-${rowPlusOne}-${colPlusOne}`;
       newRow.appendChild(individualCell);
+      terrain[individualCell.id] = {building: null };
     }
     table.appendChild(newRow);
   }
-  board.appendChild(table);
+  gameGrid.appendChild(table);
 
-  const cellTotal = document.querySelectorAll('.grid-cell');
+//Another version for event listener to trigger on cell click but not dynamic for expanding grid
+//const cellTotal = document.querySelectorAll('.grid-cell');
 
-//Event Listener
-cellTotal.forEach(individualCell => {
-  individualCell.addEventListener('click', handleCellClick);
-});
+// cellTotal.forEach(cell => {
+//   cell.addEventListener('click', handleCellClick);
+// });
 
-function handleCellClick(event){
-  const cellId = event.target.id;
-  console.log('Cell clicked:', cellId);
+// function handleCellClick(event){
+//   const cellId = event.target.id;
+//   console.log('Cell clicked:', cellId);
+// }
+
+gameGrid.addEventListener('click', handleCellClick);
+
+function handleCellClick(event) {
+  const clickedElement = event.target;
+  
+  if (clickedElement.classList.contains('grid-cell')) {
+    const cellId = clickedElement.id;
+    console.log('Cell clicked:', cellId);
+    console.log('Buildings:', terrain.building)
+  }
 }
+console.log(terrain)
 
 
 
